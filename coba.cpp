02d605e22979abbefbcd0e9f4398c5eb8262b9f0
Abitien Ahmad Fahrel <abitienfahrel@gmail.com>
2025-11-17 19:46:49 +0700

    new

---
+++ coba.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hai!" << endl;
+    cout << "Ayo belajar C++" << endl;
+    cout << "Semangat!" << endl;
+
+    return 0;
+}
