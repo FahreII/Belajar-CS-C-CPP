ee6c9ac6eb84cb9ab750f56796c8a5b4ffdfe407
Abitien Ahmad Fahrel <abitienfahrel@gmail.com>
2026-01-20 19:50:41 +0700

    up

---
+++ CPP/aritmatik_opratoer.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    printf("%d\n",3+2);
+    printf("%d\n",3-2);
+    printf("%d\n",3*3);
+    printf("%.2f\n",5/2);
+    printf("%d\n",20%3);
+}
